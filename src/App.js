1b3212ba43adc8ebc9f548d3e96dce9abd03055f
WDRK <lllrrrlrssr@gmail.com>
2021-05-14 17:22:39 +0900

    index.js에서 App에 넘긴 props를 App.js에서 받아서 Todo 컴포넌트의 props값으로 활용함

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import Todo from "./components/Todo";
 
 function App(props) {
+  const taskList = props.tasks.map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -44,9 +52,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name="Eat" completed={true} id="todo-0" />
-        <Todo name="Sleep" completed={false} id="todo-1" />
-        <Todo name="Repeat" completed={false} id="todo-2" />
+        {taskList}
       </ul>
     </div>
   );

58d43082f5a555034605cacb2804685604f927a2
WDRK <lllrrrlrssr@gmail.com>
2021-05-14 17:08:00 +0900

    completed와 id props를 추가해서 Todo.js에서 값을 활용함

--- src/App.js
+++ src/App.js
@@ -44,9 +44,9 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name="Eat" />
-        <Todo name="Sleep" />
-        <Todo name="Repeat" />
+        <Todo name="Eat" completed={true} id="todo-0" />
+        <Todo name="Sleep" completed={false} id="todo-1" />
+        <Todo name="Repeat" completed={false} id="todo-2" />
       </ul>
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,8 +4,8 @@ export default function Todo(props) {
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-        <input id="todo-0" type="checkbox" defaultChecked={true} />
-        <label htmlFor="todo-0" className="todo-label">
+        <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+        <label className="todo-label" htmlFor={props.id}>
           {props.name}
         </label>
       </div>

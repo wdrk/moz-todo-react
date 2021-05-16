08b4786dc92554ada51f14f40e7dfe41a891c6b8
WDRK <lllrrrlrssr@gmail.com>
2021-05-16 21:29:59 +0900

    isEditing 값에 따라서 Todo.js의 Todo() 함수가 editingTemplate나 viewTemplate를 리턴하도록 구성하고 Edit 버튼 클릭 이벤트를 등록함

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,7 +11,11 @@ export default function Todo(props) {
         <input id={props.id} className="todo-text" type="text" />
       </div>
       <div className="btn-group">
-        <button type="button" className="btn todo-cancel">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
           Cancel
           <span className="visually-hidden">renaming {props.name}</span>
         </button>
@@ -36,7 +40,7 @@ export default function Todo(props) {
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn">
+        <button type="button" className="btn" onClick={() => setEditing(true)}>
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
@@ -49,31 +53,5 @@ export default function Todo(props) {
       </div>
     </div>
   );
-  return (
-    <li className="todo stack-small">
-      <div className="c-cb">
-        <input
-          id={props.id}
-          type="checkbox"
-          defaultChecked={props.completed}
-          onChange={() => props.toggleTaskCompleted(props.id)}
-        />
-        <label className="todo-label" htmlFor={props.id}>
-          {props.name}
-        </label>
-      </div>
-      <div className="btn-group">
-        <button type="button" className="btn">
-          Edit <span className="visually-hidden">Eat</span>
-        </button>
-        <button
-          type="button"
-          className="btn btn__danger"
-          onClick={() => props.deleteTask(props.id)}
-        >
-          Delete <span className="visually-hidden">{props.name}</span>
-        </button>
-      </div>
-    </li>
-  );
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }

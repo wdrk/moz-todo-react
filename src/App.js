f858a910e99d43ea8f1b7dcdf21236f8f0207278
WDRK <lllrrrlrssr@gmail.com>
2021-05-16 22:08:34 +0900

    filter 버튼이 동작하도록 filter state를 만들고 filterbutton에 props를 추가, 클릭 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
+// App 컴포넌트를 렌더링 할 때마다 다시 계산하지 않기 위해서 App 외부에 선언함
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("ALL");
 
   function addTask(name) {
     const newTask = {
@@ -53,17 +63,22 @@ function App(props) {
     />
   ));
 
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    ></FilterButton>
+  ));
+
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"

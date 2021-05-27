fa44d3a2d427cdd967a5f649150c97a8a9c4fa15
WDRK <lllrrrlrssr@gmail.com>
2021-05-27 15:04:10 +0900

    Feat: 추가 App과 Form 간의 Callback prop
    App이 필요한 Form의 정보를 넘겨주기 위해서

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Form(props) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask("say hello");
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?

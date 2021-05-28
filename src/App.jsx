18be1eec2f5c81134b6f0f0f8fef190652f84be2
WDRK <lllrrrlrssr@gmail.com>
2021-05-28 14:40:34 +0900

    Feat: 추가 edit 기능
    task의 이름을 변경하기 위해

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,15 @@ function App(props) {
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
   }
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
   const taskList = tasks.map(task => {
     return (
       <Todo
@@ -32,6 +41,7 @@ function App(props) {
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
+        editTask={editTask}
       />
     );
   });

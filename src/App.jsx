5dc25295bdb01976131073b39c42861c01539c0c
WDRK <lllrrrlrssr@gmail.com>
2021-05-28 18:48:21 +0900

    Feat: 추가 FilterButton 기능
    Checkbox의 상태에 따라 보여줄 TaskList를 설정하기 위한 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App(props) {
     });
     setTasks(editedTaskList);
   }
-  const taskList = tasks.map(task => {
+  const taskList = tasks.filter(FILTER_MAP[filter]).map(task => {
     return (
       <Todo
         id={task.id}

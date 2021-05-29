b1165612a44f7190467142164b761e14e7f2cef8
WDRK <lllrrrlrssr@gmail.com>
2021-05-29 21:54:24 +0900

    Feat: 추가 상황별 edit버튼 focus
    페이지 로딩 시에는 edit 버튼에 focus가 가지 않고 cancel버튼 클릭 후에만 focus가 가도록

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
   const editFieldRef = useRef(null);
   const editButtonRef = useRef(null);
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     console.log(e.target.value);
@@ -82,8 +91,10 @@ export default function Todo(props) {
     </div>
   );
   useEffect(() => {
-    if (isEditing) {
+    if (!wasEditing && isEditing) {
       editFieldRef.current.focus();
+    } else if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
     }
   }, [isEditing]);
   console.log("main render");
